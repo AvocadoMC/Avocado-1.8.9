ffc07abafd7a6c1bddd7704b89d370b36b70dded
BeoPhiMan651 <testaccvn8172@gmail.com>
2026-03-01 15:51:29 +0700

    b1.4 working

--- src/main/java/net/ccbluex/avocado/api/ClientUpdate.kt
+++ src/main/java/net/ccbluex/avocado/api/ClientUpdate.kt
@@ -20,7 +20,7 @@ object ClientUpdate {
         if (inputStream != null) {
             it.load(inputStream)
         } else {
-            it["git.build.version"] = "B1.3"
+            it["git.build.version"] = "B1.4"
         }
     }
 
